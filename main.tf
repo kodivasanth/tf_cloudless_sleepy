76df0536e5a6b6587e95ba09ca1cd8d9b4832b95
kodivasanth <kvasanthkumar@gmail.com>
2020-07-07 11:14:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}"
   vars = {
-    sample_var = $${sample_var}
+    sample_var = "$${sample_var}"
   }
 }
 
