52f5c379a774af82430d47b98b1e585e84e8d56a
kodivasanth <kvasanthkumar@gmail.com>
2020-07-07 10:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 # Create an arbitrary local resource
 data "template_file" "test" {
   template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars {
+  vars = {
     sample_var = var.sample_var
   }
 }
